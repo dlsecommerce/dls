a920174c06e4a2452850d2a5e9bd9a3bc81fc818
DLS ECOMMERCE <faturamentopikot@gmail.com>
2025-10-07 16:26:33 -0300

    Atualizando

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Evita abrir uma janela de console no Windows em modo release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  dls_ecommerce_lib::run();
+    dls_ecommerce_lib::run();
 }
